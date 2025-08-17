dd4c16fbf49e53428438f7b326a56def9154814c
Perry Huang <perryhuang2015@gmail.com>
2025-08-17 16:03:41 -0400

    feat: add styling for error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,14 @@
 import NavBar from "../NavBar/NavBar";
+import styles from "./styles.module.css";
 
 const ErrorPage = () => {
     return (
         <>
             <NavBar />
-            <p>404 error! Probably</p>
-            <p>This page will return an actual error later</p>
+            <p className={styles.error}>This page doesn't exist!</p>
+            <p className={styles.error}>
+                Click on Home or Store to go to a page that does.
+            </p>
         </>
     );
 };
