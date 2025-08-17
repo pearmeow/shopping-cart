4b43aa0729c0c190ff3f0fdd877bcbd3977b3119
Perry Huang <perryhuang2015@gmail.com>
2025-08-17 16:18:43 -0400

    feat: style main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-import "./styles/App.css";
+import styles from "./styles.module.css";
 
 function App() {
     return (
         <>
             <NavBar />
-            <h1>Welcome to InsertStoreBrandNameHere</h1>
-            <h2>
+            <h1 className={styles.header1}>
+                Welcome to InsertStoreBrandNameHere
+            </h1>
+            <h2 className={styles.header2}>Yes, that's actually our name</h2>
+            <h3 className={styles.header3}>
                 <Link to="/store">Visit our store and buy something now!</Link>
-            </h2>
+            </h3>
         </>
     );
 }
